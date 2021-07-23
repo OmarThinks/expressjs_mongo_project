4f2ab59858ab400ea9c29d24c7d779042a2f4c5d
Omar Magdy <contact.parmag@gmail.com>
2021-07-23 03:04:13 +0200

    index.js:add:products list endpoint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 const express = require('express')
 const app = express()
  
+
+/*Importing Models*/
+const {Product} = require("./schemas/_db.js");
+
+
+
+
 app.get('/', function (req, res) {
   res.send('Hello World')
-})
+});
+
+
+/*
+app.get('/products', function (req, res) {
+  
+
+
+	res.send('Hello World');
+
+});
+
+*/
+
+
+app.get('/products', function(req, res) {
+  Product.find({}, function(err, products) {
+    var productMap = {};
+
+    products.forEach(function(product) {
+      productMap[product._id] = product;
+    });
+
+    res.send(productMap);  
+  });
+});
+
+
+
  
 app.listen(3000)
