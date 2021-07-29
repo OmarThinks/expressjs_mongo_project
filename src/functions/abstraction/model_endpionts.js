95c2c551c67354177bf171c06df0e9e3ba0735d7
Omar Magdy <contact.parmag@gmail.com>
2021-07-29 10:05:58 +0200

    abstraction:model_enspoints:add list

---
+++ src/functions/abstraction/model_endpionts.js
@@ -0,0 +1,19 @@
+function listEndPoint(model)
+{
+	function endPointToReturn(req,res)
+	{
+		model.find({}, function(err, documents) 
+		{
+		    var docMap = {};
+
+		    documents.forEach(function(doc) 
+		    {
+		      docMap[doc._id] = doc;
+		    });
+
+		    res.send(docMap);  
+		});
+	}
+
+	return endPointToReturn;
+}
